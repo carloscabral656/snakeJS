7133191185a05c144f762ea528fb2b2eb8645b46
carloscabral656 <edu.carlos.cec@gmail.com>
2024-06-08 23:07:18 -0300

    Creating new styles

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 import Scenario from "./entities/Scenario.js"
 import Snake from "./entities/Snake.js"
 
-let scenario = new Scenario(50, 100, window)
+let scenario = new Scenario(50, 70, window)
 scenario.constructScenarioStruct()
 scenario.renderScenario()
 scenario.createBoundaries()
 scenario.createAvailableScenarioApple()
+
+
 let snake = new Snake()
 scenario.setSnake(snake)
 scenario.createCrawl()
 scenario.createListeners()
 scenario.spawSnake()
 scenario.spawApple()
+
+
+
 
